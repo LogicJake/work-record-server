7229064f52ecd1da69c4df3d23b2d1d9ac18fb2b
ubuntu <835410808@qq.com>
2018-05-12 21:51:52 +0800

    发布工作添加开始日期

--- README.md
+++ README.md
@@ -168,10 +168,12 @@ http://api.logicjake.xyz/work-record/?_action=releaseWork
 |wages|[{"type":"家装主材安装","wage":100},{"type":"电器安装维修","wage":110}]|
 |house|板房|
 |welfare|高温补贴|
+|start_time|20180611|
 ### 说明
 * wages为招聘工种和对应工资  
 * house为住房条件
-* welfare为福利 
+* welfare为福利   
+* start_time开始时间
 ### 返回值
 #### 成功，status=1
 ```
--- actions/releaseWork.php
+++ actions/releaseWork.php
@@ -2,8 +2,8 @@
 
 require_once './include/company.function.php';
 
-if(isset($_GET['address'],$_GET['phone'],$_GET['wages'],$_GET['house'],$_GET['welfare'])){
-	$res = releaseWork($_GET['address'],$_GET['phone'],$_GET['wages'],$_GET['house'],$_GET['welfare']);
+if(isset($_GET['address'],$_GET['phone'],$_GET['wages'],$_GET['house'],$_GET['welfare'],$_GET['start_time'])){
+	$res = releaseWork($_GET['address'],$_GET['phone'],$_GET['wages'],$_GET['house'],$_GET['welfare'],$_GET['start_time']);
 	Result::success($res);
 }
 else
